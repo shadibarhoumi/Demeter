f56c74797ed8b3e82c8d67f06a916b27a30e6ca9
Shadi Barhoumi <shadibarhoumi@gmail.com>
2021-04-09 20:31:22 -0600

    Basic profile page working

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,8 +1,24 @@
+import { IntervalStats } from '@features/dashboard/IntervalStats'
+import { firestore } from '@lib/firebase'
 import { useRouter } from 'next/router'
+import React from 'react'
+import { useDocument, useDocumentDataOnce } from 'react-firebase-hooks/firestore'
 
 export default function ProfilePage({}) {
   const router = useRouter()
   const { username } = router.query
+  if (!username) return <p>404</p>
 
-  return <main>{username}</main>
+  const usernamesRef = firestore.collection('usernames').doc(username as string)
+  const [usernameObject] = useDocumentDataOnce(usernamesRef)
+
+  if (!usernameObject) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <main>
+      <IntervalStats userId={usernameObject.uid} />
+    </main>
+  )
 }
