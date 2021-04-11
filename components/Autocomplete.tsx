d092e654afc788407616f6934ff9ada855eae856
Shadi Barhoumi <shadibarhoumi@gmail.com>
2021-04-10 20:43:12 -0600

    First version of autocomplete working

--- components/Autocomplete.tsx
+++ components/Autocomplete.tsx
@@ -1,16 +1,62 @@
-import { CheckBoxOutlineBlankRounded } from '@material-ui/icons'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const Option = styled.div`
+const OptionDiv = styled.div`
   padding-left: 5px;
 
   &:hover {
     background-color: lightgray;
   }
 `
+
+const Option = ({
+  option,
+  matchIndices,
+  onSelect,
+}: {
+  option: string
+  matchIndices: number[]
+  onSelect: () => void
+}) => {
+  let part = ''
+  let curInd = 0
+  let toRender: JSX.Element[] = []
+  for (let i = 0; i < option.length; i++) {
+    if (i !== matchIndices[curInd]) {
+      part += option[i]
+    } else {
+      if (part) toRender.push(<span key={i - 1}>{part}</span>)
+      toRender.push(
+        <span key={i} style={{ fontWeight: 'bold' }}>
+          {option[i]}
+        </span>,
+      )
+      part = ''
+      curInd++
+    }
+  }
+  if (part) toRender.push(<span key={option.length}>{part}</span>)
+  return (
+    <OptionDiv key={option} onMouseDown={onSelect}>
+      {toRender}
+    </OptionDiv>
+  )
+}
+
 const matchStrings = (prefix: string, candidate: string) => {
-  return candidate.substring(0, prefix.length) === prefix
+  let j = 0 // index into candidate string
+  let matchIndices = []
+  for (let i = 0; i < prefix.length; i++) {
+    while (candidate[j] !== prefix[i]) {
+      j++
+      if (j >= candidate.length) {
+        return { isMatch: false, matchIndices }
+      }
+    }
+    matchIndices.push(j)
+    j++
+  }
+  return { isMatch: true, matchIndices }
 }
 
 const OptionsList = ({
@@ -22,7 +68,9 @@ const OptionsList = ({
   input: string
   handleSelect: (option: string) => void
 }) => {
-  const matchingOptions = options.filter((option) => matchStrings(input, option))
+  let matches = options.map((option) => matchStrings(input, option))
+  const matchingOptions = options.filter((_, i) => matches[i].isMatch)
+  matches = matches.filter((match) => match.isMatch)
   if (!matchingOptions.length) return null
 
   return (
@@ -34,21 +82,33 @@ const OptionsList = ({
         border: 'solid 1px black',
         zIndex: 99,
       }}
-      onClick={() => {
-        console.log('clicked outer')
-      }}
     >
-      {matchingOptions.map((option) => (
-        <Option key={option} onMouseDown={() => handleSelect(option)}>
-          {option}
-        </Option>
+      {matchingOptions.map((option, i) => (
+        <Option
+          key={option}
+          option={option}
+          matchIndices={matches[i].matchIndices}
+          onSelect={() => handleSelect(option)}
+        />
       ))}
     </div>
   )
 }
 
 export const Autocomplete: React.FC = () => {
-  const options = ['wuyan', 'shadi', 'jim', 'bob', 'rachel']
+  const options = [
+    'wuyan',
+    'shadi',
+    'samuel',
+    'shaker',
+    'shannon',
+    'shearer',
+    'wuming',
+    'wooyan',
+    'jim',
+    'bob',
+    'rachel',
+  ]
   const [isFocused, setIsFocused] = useState(false)
   const [selected, setSelected] = useState('')
   const displayDropdown = isFocused && selected.length > 1
